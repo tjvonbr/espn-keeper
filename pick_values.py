11e5f74d232cabe8af46d4c2c69bde5efd28abdb
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2021-01-09 23:41:56 -0600

    initial commit

---
+++ pick_values.py
@@ -0,0 +1,290 @@
+pick_values = [
+    {
+        "overall": 1,
+        "round": 1,
+        "pick": 1,
+        "value": 335.2
+    },
+    {
+        "overall": 2,
+        "round": 1,
+        "pick": 2,
+        "value": 310.0
+    },
+    {
+        "overall": 3,
+        "round": 1,
+        "pick": 3,
+        "value": 293.6
+    },
+    {
+        "overall": 4,
+        "round": 1,
+        "pick": 4,
+        "value": 282.0
+    },
+    {
+        "overall": 5,
+        "round": 1,
+        "pick": 5,
+        "value": 273.2
+    },
+    {
+        "overall": 6,
+        "round": 1,
+        "pick": 6,
+        "value": 268.0
+    },
+    {
+        "overall": 7,
+        "round": 1,
+        "pick": 7,
+        "value": 261.4
+    },
+    {
+        "overall": 8,
+        "round": 1,
+        "pick": 8,
+        "value": 257.2
+    },
+    {
+        "overall": 9,
+        "round": 1,
+        "pick": 9,
+        "value": 252.6
+    },
+    {
+        "overall": 10,
+        "round": 1,
+        "pick": 10,
+        "value": 249.4
+    },
+    {
+        "overall": 11,
+        "round": 1,
+        "pick": 11,
+        "value": 241.6
+    },
+    {
+        "overall": 12,
+        "round": 1,
+        "pick": 12,
+        "value": 237.2
+    },
+    {
+        "overall": 13,
+        "round": 2,
+        "pick": 1,
+        "value": 233.4
+    },
+    {
+        "overall": 14,
+        "round": 2,
+        "pick": 2,
+        "value": 229.4
+    },
+    {
+        "overall": 15,
+        "round": 2,
+        "pick": 3,
+        "value": 226.4
+    },
+    {
+        "overall": 16,
+        "round": 2,
+        "pick": 4,
+        "value": 222.6
+    },
+    {
+        "overall": 17,
+        "round": 2,
+        "pick": 5,
+        "value": 219.8
+    },
+    {
+        "overall": 18,
+        "round": 2,
+        "pick": 6,
+        "value": 217.0
+    },
+    {
+        "overall": 19,
+        "round": 2,
+        "pick": 7,
+        "value": 212.8
+    },
+    {
+        "overall": 20,
+        "round": 2,
+        "pick": 8,
+        "value": 210.2
+    },
+    {
+        "overall": 21,
+        "round": 2,
+        "pick": 9,
+        "value": 206.6
+    },
+    {
+        "overall": 22,
+        "round": 2,
+        "pick": 10,
+        "value": 204.8
+    },
+    {
+        "overall": 23,
+        "round": 2,
+        "pick": 11,
+        "value": 201.6
+    },
+    {
+        "overall": 24,
+        "round": 2,
+        "pick": 12,
+        "value": 198.8
+    },
+    {
+        "overall": 25,
+        "round": 3,
+        "pick": 1,
+        "value": 194.8
+    },
+    {
+        "overall": 26,
+        "round": 3,
+        "pick": 2,
+        "value": 192.0
+    },
+    {
+        "overall": 27,
+        "round": 3,
+        "pick": 3,
+        "value": 190.6
+    },
+    {
+        "overall": 28,
+        "round": 3,
+        "pick": 4,
+        "value": 188.0
+    },
+    {
+        "overall": 29,
+        "round": 3,
+        "pick": 5,
+        "value": 184.2
+    },
+    {
+        "overall": 30,
+        "round": 3,
+        "pick": 6,
+        "value": 182.4
+    },
+    {
+        "overall": 31,
+        "round": 3,
+        "pick": 7,
+        "value": 180.0
+    },
+    {
+        "overall": 32,
+        "round": 3,
+        "pick": 8,
+        "value": 176.8
+    },
+    {
+        "overall": 33,
+        "round": 3,
+        "pick": 9,
+        "value": 174.8
+    },
+    {
+        "overall": 34,
+        "round": 3,
+        "pick": 10,
+        "value": 173.6
+    },
+    {
+        "overall": 35,
+        "round": 3,
+        "pick": 11,
+        "value": 172.0
+    },
+    {
+        "overall": 36,
+        "round": 3,
+        "pick": 12,
+        "value": 168.4
+    },
+    {
+        "overall": 37,
+        "round": 4,
+        "pick": 1,
+        "value": 166.6
+    },
+    {
+        "overall": 38,
+        "round": 4,
+        "pick": 2,
+        "value": 164.6
+    },
+    {
+        "overall": 39,
+        "round": 4,
+        "pick": 3,
+        "value": 162.4
+    },
+    {
+        "overall": 40,
+        "round": 4,
+        "pick": 4,
+        "value": 159.0
+    },
+    {
+        "overall": 41,
+        "round": 4,
+        "pick": 5,
+        "value": 156.6
+    },
+    {
+        "overall": 42,
+        "round": 4,
+        "pick": 6,
+        "value": 154.8
+    },
+    {
+        "overall": 43,
+        "round": 4,
+        "pick": 7,
+        "value": 153.2
+    },
+    {
+        "overall": 44,
+        "round": 4,
+        "pick": 8,
+        "value": 150.4
+    },
+    {
+        "overall": 45,
+        "round": 4,
+        "pick": 9,
+        "value": 147.4
+    },
+    {
+        "overall": 46,
+        "round": 4,
+        "pick": 10,
+        "value": 145.4
+    },
+    {
+        "overall": 47,
+        "round": 4,
+        "pick": 11,
+        "value": 143.4
+    },
+    {
+        "overall": 48,
+        "round": 4,
+        "pick": 12,
+        "value": 141.8
+    }
+]
---
+++ players.py
@@ -0,0 +1,47 @@
+roster = {
+    "Justin Herbert": {
+        "projected_points": 163.88,
+    },
+    "J.K. Dobbins": {
+        "projected_points": 110.27,
+    },
+    "Christian McCaffrey": {
+        "projected_points": 340.97,
+    },
+    "Chris Godwin": {
+        "projected_points": 244.4,
+    },
+    "Adam Thielen": {
+        "projected_points": 230.88,
+    },
+    "Eric Ebron": {
+        "projected_points": 74.52,
+    },
+    "Myles Gaskin": {
+        "projected_points": 16.46,
+    },
+    "DeVante Parker": {
+        "projected_points": 137.87,
+    },
+    "Bucaneers D/ST": {
+        "projected_points": 126.58,
+    },
+    "Daniel Carlson": {
+        "projected_points": 114.43,
+    }
+}
+
+# def highestScorer(team):
+#     highest_scorer = ""
+#     highest_score = 0
+
+#     for player in team:
+#         if player["projected_points"] > highest_score:
+#             highest_score = player["projected_points"]
+#             highest_scorer = player["name"]
+
+#     return (highest_scorer, highest_score)
+
+# print(highestScorer(roster))
+
+    
